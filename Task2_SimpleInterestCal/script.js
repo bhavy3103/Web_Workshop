69a14fce37e091a83d015657f269c131e1e82379
Bhavy <bhavypdevani31@gmail.com>
2023-08-22 22:45:49 +0530

    init

---
+++ Task2_SimpleInterestCal/script.js
@@ -0,0 +1,10 @@
+document.getElementById('calculate').addEventListener('click', function() {
+    const principal = parseFloat(document.getElementById('principal').value);
+    const rate = parseFloat(document.getElementById('rate').value);
+    const time = parseFloat(document.getElementById('time').value);
+  
+    const simpleInterest = (principal * rate * time) / 100;
+    
+    document.getElementById('result').textContent = `Simple Interest: ${simpleInterest.toFixed(2)}`;
+  });
+  
